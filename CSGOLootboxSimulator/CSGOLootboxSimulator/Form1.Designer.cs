cd7c5ae47fdc00dc6d67c5b7aa94ec6826d07e41
Arne De Klerk <33641372+Arnedeklerk@users.noreply.github.com>
2022-03-21 10:18:59 +0000

    Initial upload
    Full, working program

---
+++ CSGOLootboxSimulator/CSGOLootboxSimulator/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CSGOLootboxSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userIterations = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.knifeCount = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.outcomeBoxTwo = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(179, 61);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Go!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Arné\'s CSGO Lootbox Simulator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // userIterations
+            // 
+            this.userIterations.Location = new System.Drawing.Point(73, 63);
+            this.userIterations.Name = "userIterations";
+            this.userIterations.Size = new System.Drawing.Size(100, 20);
+            this.userIterations.TabIndex = 3;
+            this.userIterations.TextChanged += new System.EventHandler(this.UserIterations_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Iterations:";
+            // 
+            // knifeCount
+            // 
+            this.knifeCount.AutoSize = true;
+            this.knifeCount.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.knifeCount.Location = new System.Drawing.Point(393, 95);
+            this.knifeCount.Name = "knifeCount";
+            this.knifeCount.Size = new System.Drawing.Size(0, 27);
+            this.knifeCount.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(376, 890);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // outcomeBoxTwo
+            // 
+            this.outcomeBoxTwo.Font = new System.Drawing.Font("Gill Sans MT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outcomeBoxTwo.Location = new System.Drawing.Point(17, 89);
+            this.outcomeBoxTwo.Name = "outcomeBoxTwo";
+            this.outcomeBoxTwo.Size = new System.Drawing.Size(353, 935);
+            this.outcomeBoxTwo.TabIndex = 9;
+            this.outcomeBoxTwo.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 1037);
+            this.Controls.Add(this.outcomeBoxTwo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.knifeCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userIterations);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CSGO Lootbox Sim";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userIterations;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label knifeCount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox outcomeBoxTwo;
+    }
+}
+
---
+++ CSGOLootboxSimulator/CSGOLootboxSimulator/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSGOLootboxSimulator
+{
+    public partial class Form1 : Form
+    {
+        double milSpec, restricted, classified, covert;
+        int userAmount, knifeCounter, covertCounter, classifiedCounter, restrictedCounter, milspecCounter, totalCounter;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //string userInput;
+
+        private void OutcomeBox_Click(object sender, EventArgs e)
+        {
+
+        }   
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //outcomeBox.Text = ("Welcome");
+            outcomeBoxTwo.ScrollBars = RichTextBoxScrollBars.Vertical;
+            outcomeBoxTwo.Multiline = true;
+
+        }
+
+        private void UserIterations_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        Random random = new Random();
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            
+            milSpec = 79.92;
+            restricted = 15.98;
+            classified = 3.2;
+            covert = 0.64;
+            //exceedinglyRare = 0.26
+
+            //userInput = Console.ReadLine();
+            if (userIterations.Text == "")
+            {
+                outcomeBoxTwo.Text = "Please enter an iteration count";
+                
+            }
+            else
+            {
+                userAmount = Convert.ToInt32(userIterations.Text);
+
+
+                for (int j = 0; j < userAmount; j++)
+                {
+                    double percent = random.NextDouble() * 100;
+
+                    totalCounter++;
+
+                    if (percent <= milSpec)
+                    {
+                        //outcomeBoxTwo.SelectionColor = Color.Red;
+                        outcomeBoxTwo.Text += ("Milspec -\r\n");
+                        
+                        milspecCounter++;
+                    }
+
+                    else if (percent <= restricted + milSpec)
+                    {
+                        outcomeBoxTwo.Text += ("Restricted ----\r\n");
+                        restrictedCounter++;
+                    }
+
+                    else if (percent <= restricted + milSpec + classified)
+                    {
+                        outcomeBoxTwo.Text += ("Classified ------\r\n");
+                        classifiedCounter++;
+                    }
+
+                    else if (percent <= restricted + milSpec + classified + covert)
+                    {
+                        outcomeBoxTwo.Text += ("Covert --------\r\n");
+                        covertCounter++;
+                    }
+                    else
+                    {
+                        outcomeBoxTwo.Text += ("Knife / Gloves! ------------\r\n");
+                        knifeCounter++;
+                    }
+
+                    knifeCount.Text = "Milspec: " + milspecCounter.ToString() + "\r\nRestricted: " + restrictedCounter + "\r\nClassified: " + classifiedCounter + "\r\nCovert: " + covertCounter + "\r\nKnives: " + knifeCounter + "\r\nTotal: " + totalCounter;
+
+                    //foreach(int av in outcomeBoxTwo.Text)
+                    //{
+                    //    if (av = "Knife / Gloves! That's as good as it gets!")
+                    //    {
+                    //        int knifeCount++;
+                    //    }
+                    //}
+                }
+            }
+        }
+    }
+}
